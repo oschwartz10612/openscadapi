b38a2fe608ec66e957e28d0812820aa6615854e1
Owen Schwartz <oschwartz10612@gmail.com>
2020-06-04 18:15:37 -0400

    Still truomg tp fox

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -4,6 +4,6 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASS" \
---output $HOME/firebase_key.json firebase_key.json.gpg
+--output firebase_key.json firebase_key.json.gpg
 
-echo $HOME/firebase_key.json
+echo ls
